bfeec95ac1727993565fa2103011c5fd8cbc2f97
ASUS <afaringhazal@gmail.com>
2022-01-27 02:43:08 +0330

    help and fullscreen and ...

--- src/main/java/org/runApp/Designed_MenuController.java
+++ src/main/java/org/runApp/Designed_MenuController.java
@@ -4,9 +4,15 @@ import com.google.gson.internal.LinkedTreeMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +23,12 @@ public class Designed_MenuController {
 
     @FXML
     private ScrollPane ShowSection;
+
+    @FXML
+    private MenuItem idDarkMode;
+
+    @FXML
+    private MenuItem idLightMode;
 
 
     public void initialize() throws IOException {
@@ -89,6 +101,72 @@ public class Designed_MenuController {
         App.setScene(root);
 
     }
+
+
+
+    @FXML
+    void ButtonHelp(ActionEvent event) {
+        Label label = new Label("Log in : ");
+        Label label1 = new Label("sign up :");
+        Label label2= new Label("exit : ");
+        Label label3 = new Label("Like :");
+        Label label4 = new Label("Retweet : ");
+        Label label5 = new Label("follow : ");
+        Label label6 = new Label("un follow :");
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(label,label1,label2,label3,label4,label5,label6);
+
+        Scene scene = new Scene(vBox);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    void ButtonAbout(ActionEvent event) {
+
+        Label label = new Label( " Mohammad hadi Shekholeslami : 9931097 , hafezsheikh@gmail.com");
+        Label label1 = new Label("Rezvan Afari : 9827003 , afaringhazal@gmail.com ");
+        VBox vBox = new VBox();
+        vBox.getChildren().add(label);
+        vBox.getChildren().add(label1);
+
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(10));
+        vBox.setAlignment(Pos.TOP_CENTER);
+
+
+
+        Scene scene = new Scene(vBox);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    void fullScreen(ActionEvent event) {
+
+        App.f().setFullScreen(!App.f().isFullScreen());
+
+    }
+
+    @FXML
+    void DarkMode(ActionEvent event) {
+
+        Scene scene = App.f().getScene();
+        scene.getRoot().setStyle("-fx-base:black");
+
+    }
+
+    @FXML
+    void LightMode(ActionEvent event) {
+        Scene scene = App.f().getScene();
+        scene.getRoot().setStyle("");
+    }
+
 
 
 }
