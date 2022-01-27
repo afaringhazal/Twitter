4cb793e9224bea397d846b1d837eaaf4a9fb126f
ASUS <afaringhazal@gmail.com>
2022-01-27 22:37:55 +0330

    fix bug

--- src/main/java/org/runApp/Designed_MenuController.java
+++ src/main/java/org/runApp/Designed_MenuController.java
@@ -8,14 +8,20 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.TreeMap;
 
 public class Designed_MenuController {
@@ -30,13 +36,31 @@ public class Designed_MenuController {
     @FXML
     private MenuItem idLightMode;
 
+    @FXML
+    private Button idButtonCreatTweet;
+
+    @FXML
+    private Button idButtonSearch;
+
+    @FXML
+    private Button idButtonShowPage;
+
+
+    @FXML
+    private Button idButtonTimeLine;
 
     public void initialize() throws IOException {
       //  ShowSection.getChildrenUnmodifiable().add(fxmlCommandParserServiceImpl.requestTimeline());
 
         //ShowSection.getChildrenUnmodifiable()
         ShowSection.setContent(fxmlCommandParserServiceImpl.requestTimeline());
-       // App.f().show();
+        setSaveAccelerator(idButtonTimeLine);
+        setSaveAccelerator1(idButtonShowPage);
+        setSaveAccelerator2(idButtonSearch);
+        setSaveAccelerator3(idButtonCreatTweet);
+
+
+        // App.f().show();
 
        // Stage s = (Stage) ShowSection.getScene().getWindow();
 
@@ -106,16 +130,23 @@ public class Designed_MenuController {
 
     @FXML
     void ButtonHelp(ActionEvent event) {
-        Label label = new Label("Log in : ");
-        Label label1 = new Label("sign up :");
-        Label label2= new Label("exit : ");
-        Label label3 = new Label("Like :");
-        Label label4 = new Label("Retweet : ");
-        Label label5 = new Label("follow : ");
-        Label label6 = new Label("un follow :");
+        Label label = new Label("Log in : You can enter by pressing this key  ");
+        Label label1 = new Label("sign up : register");
+        Label label2= new Label("exit :Exit the application");
+        Label label3 = new Label("Like : Favorite a tweet ");
+        Label label4 = new Label("Retweet : Repost a tweet ");
+        Label label5 = new Label("follow : follow a person and see their tweets ");
+        Label label6 = new Label("un follow : un follow a person ");
+        Label label7 = new Label("Open time line : ctrl + s");
+
+        Label label8 = new Label("Open page : ctrl + n");
+
+        Label label9 = new Label("Open search : ctrl + p");
+        Label label10 = new Label("Creat tweet  : ctrl +z");
+
 
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(label,label1,label2,label3,label4,label5,label6);
+        vBox.getChildren().addAll(label,label1,label2,label3,label4,label5,label6,label7,label8,label9,label10);
 
         Scene scene = new Scene(vBox);
         Stage stage = new Stage();
@@ -167,6 +198,89 @@ public class Designed_MenuController {
         scene.getRoot().setStyle("");
     }
 
+//    @FXML
+//    void ButtonTimeLine(KeyEvent event) {
+//
+//        if(event.getText().equals('a')){
+//            System.out.println("----");
+//
+//        }
+//
+//
+//        App.f().getScene().setOnKeyPressed(e -> {
+//            if (e.getCode() == KeyCode.A) {
+//                System.out.println("A key was pressed");
+//            }
+//        });
+//
+//    }
+//
+//
+//    @FXML
+//    private void initialize() throws SQLException, ClassNotFoundException, IOException {
+//        setSaveAccelerator(idButtonTimeLine);
+//    }
 
+//
+    private void setSaveAccelerator(final Button button) {
+        Scene scene = App.f().getScene();
+        if (scene == null) {
+            throw new IllegalArgumentException("setSaveAccelerator must be called when a button is attached to a scene");
+        }
 
+        scene.getAccelerators().put(
+                new KeyCodeCombination(KeyCode.S, KeyCombination.SHORTCUT_DOWN),
+                new Runnable() {
+                    @FXML public void run() {
+                        button.fire();
+                    }
+                }
+        );
+    }
+
+    private void setSaveAccelerator1(final Button button) {
+        Scene scene = App.f().getScene();
+        if (scene == null) {
+            throw new IllegalArgumentException("setSaveAccelerator must be called when a button is attached to a scene");
+        }
+
+        scene.getAccelerators().put(
+                new KeyCodeCombination(KeyCode.N, KeyCombination.SHORTCUT_DOWN),
+                new Runnable() {
+                    @FXML public void run() {
+                        button.fire();
+                    }
+                }
+        );
+    }
+    private void setSaveAccelerator2(final Button button) {
+        Scene scene = App.f().getScene();
+        if (scene == null) {
+            throw new IllegalArgumentException("setSaveAccelerator must be called when a button is attached to a scene");
+        }
+
+        scene.getAccelerators().put(
+                new KeyCodeCombination(KeyCode.P, KeyCombination.SHORTCUT_DOWN),
+                new Runnable() {
+                    @FXML public void run() {
+                        button.fire();
+                    }
+                }
+        );
+    }
+    private void setSaveAccelerator3(final Button button) {
+        Scene scene = App.f().getScene();
+        if (scene == null) {
+            throw new IllegalArgumentException("setSaveAccelerator must be called when a button is attached to a scene");
+        }
+
+        scene.getAccelerators().put(
+                new KeyCodeCombination(KeyCode.Z, KeyCombination.SHORTCUT_DOWN),
+                new Runnable() {
+                    @FXML public void run() {
+                        button.fire();
+                    }
+                }
+        );
+    }
 }
